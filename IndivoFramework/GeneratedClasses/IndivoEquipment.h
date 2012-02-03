73f932d718c8b22d59a633f53f0a4bcc15382c83
Pascal Pfiffner <phase.of.matter@gmail.com>
2012-02-02 20:06:32 -0500

    Add missing files :)

---
+++ IndivoFramework/GeneratedClasses/IndivoEquipment.h
@@ -0,0 +1,44 @@
+/*
+ IndivoEquipment.h
+ IndivoFramework
+ 
+ Created by Indivo Class Generator on 2/2/2012.
+ Copyright (c) 2012 Children's Hospital Boston
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#import "IndivoDocument.h"
+#import "INObjects.h"
+
+
+	
+
+/**
+ *	A class representing "indivo:Equipment" objects, generated from equipment.xsd.
+ */
+@interface IndivoEquipment : IndivoDocument
+
+@property (nonatomic, strong) INDate *dateStarted;
+@property (nonatomic, strong) INDate *dateStopped;
+@property (nonatomic, strong) INString *type;
+@property (nonatomic, strong) INString *name;					///< Must not be nil nor return YES on isNull (minOccurs = 1)
+@property (nonatomic, strong) INString *vendor;
+@property (nonatomic, strong) INString *description;
+@property (nonatomic, strong) INString *specification;
+@property (nonatomic, strong) INString *certification;
+
+
+@end
---
+++ IndivoFramework/GeneratedClasses/IndivoEquipment.m
@@ -0,0 +1,78 @@
+/*
+ IndivoEquipment.m
+ IndivoFramework
+ 
+ Created by Indivo Class Generator on 2/2/2012.
+ Copyright (c) 2012 Children's Hospital Boston
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#import "IndivoEquipment.h"
+
+
+@implementation IndivoEquipment
+
+@synthesize dateStarted, dateStopped, type, name, vendor, description, specification, certification;
+
+
++ (NSString *)nodeName
+{
+	return @"Equipment";
+}
+
++ (NSString *)nodeType
+{
+	return @"indivo:Equipment";
+}
+
++ (NSDictionary *)propertyClassMapper
+{
+	return [NSDictionary dictionaryWithObjectsAndKeys:
+			@"INDate", @"dateStarted",
+			@"INDate", @"dateStopped",
+			@"INString", @"type",
+			@"INString", @"name",
+			@"INString", @"vendor",
+			@"INString", @"description",
+			@"INString", @"specification",
+			@"INString", @"certification",
+			nil];
+}
+
++ (NSArray *)nonNilPropertyNames
+{
+	return [NSArray arrayWithObjects:@"name", nil];
+	/*
+	static NSArray *nonNilPropertyNames = nil;
+	if (!nonNilPropertyNames) {
+		nonNilPropertyNames = [[NSArray alloc] initWithObjects:@"name", nil];
+	}
+	
+	return nonNilPropertyNames;	*/
+}
+
++ (NSArray *)attributeNames
+{
+	NSArray *myAttributes = [NSArray arrayWithObjects:nil, nil];
+	NSArray *superAttr = [super attributeNames];
+	if (superAttr) {
+		myAttributes = [superAttr arrayByAddingObjectsFromArray:myAttributes];
+	}
+	return myAttributes;
+}
+
+
+@end
